40e99c6325a153009cb0036bb1c4fa2376faeef4
dabakeketan <dabakeketan@gmail.com>
2024-01-17 20:45:41 +0530

    environment update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  baseAppUrl: 'https://1nmhzcney8.execute-api.us-west-2.amazonaws.com/Dev/',
+  baseAppUrl: 'https://rgmq0t1im4.execute-api.us-west-2.amazonaws.com/Prod/',
   loginPortalUrl: 'https://crexnmsdev1.solint.net/portal',
-  mngAppBaseUrl: 'https://1nmhzcney8.execute-api.us-west-2.amazonaws.com/Dev/',
-  manageAuthUrl: 'https://crexappstore-dev.us.auth0.com/v2/',
-  mngAppAuth0ClientId: 'C8ny9GVbOzcyA8CtFI75D0QzUppp8vi5'
+  mngAppBaseUrl: 'https://rgmq0t1im4.execute-api.us-west-2.amazonaws.com/Prod/',
+  manageAuthUrl: 'https://crexappstore-dev.us.auth0.com/v2/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,7 @@ export const environment = {
   baseAppUrl: 'https://1nmhzcney8.execute-api.us-west-2.amazonaws.com/Dev/',
   loginPortalUrl: 'https://crexnmsdev1.solint.net/portal',
   mngAppBaseUrl: 'https://1nmhzcney8.execute-api.us-west-2.amazonaws.com/Dev/',
-  manageAuthUrl: 'https://crexappstore-dev.us.auth0.com/v2/',
-  mngAppAuth0ClientId: 'C8ny9GVbOzcyA8CtFI75D0QzUppp8vi5'
+  manageAuthUrl: 'https://crexappstore-dev.us.auth0.com/v2/'
 };
 
 /*
